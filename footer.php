158338ffb85b4beaba6d1dddf0dd19276f69bec4
pierre <pierre@156c5556-8ead-41bd-8221-c85054dfef69>
2009-10-11 23:06:16 +0000

    Ajout du thème de base
    git-svn-id: http://svn.pierrebertet.net/lesintegristes_wptheme/trunk@2 156c5556-8ead-41bd-8221-c85054dfef69

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Starkers
+ */
+?>
+  
+    <p><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a> <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
+		
+		<?php wp_footer(); ?>
+  </div>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Starkers
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+	<div id="wrapper">
+		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+		<p><?php bloginfo('description'); ?></p>
