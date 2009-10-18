cfd3c16509d73961d0cb0b101ab65e1de6a87246
pierre <pierre@156c5556-8ead-41bd-8221-c85054dfef69>
2009-10-18 23:18:14 +0000

    Ajout : Template formulaire + Images
    git-svn-id: http://svn.pierrebertet.net/lesintegristes_wptheme/trunk@5 156c5556-8ead-41bd-8221-c85054dfef69

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form method="get" id="searchform" action="http://lesintegristes.dev/" role="search">
+	<div>
+		<label class="screen-reader-text" for="s">Recherche pour:</label>
+		<input type="text" value="" name="s" id="s" />
+		<button type="submit" id="searchsubmit"><span><span>Recherche</span></span></button>
+	</div>
+</form>
