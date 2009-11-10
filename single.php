be2826740849efa18d5adeee4a5e031d23d4b91a
pierre <pierre@156c5556-8ead-41bd-8221-c85054dfef69>
2009-11-10 20:22:58 +0000

    Article seul : texte du footer
    git-svn-id: http://svn.pierrebertet.net/lesintegristes_wptheme/trunk@15 156c5556-8ead-41bd-8221-c85054dfef69

--- single.php
+++ single.php
@@ -19,28 +19,19 @@ get_header(); ?>
 				<p class="author">Par <strong><?php the_author() ?></strong></p>
 			</header>
 			<div class="content">
-				<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
+				<?php the_content(); ?>
 			</div>
 			<footer>
 				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 				
-				<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-				<p class="read-post"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">Lire la suite</a></p>
-				<p class="comments"><?php comments_popup_link('<strong><span>0</span></strong> <span>commentaires</span>', '<strong><span>1</span></strong> <span>commentaire</span>', '<strong><span>%</span></strong> <span>commentaires</span>'); ?></p>
-				<?php //edit_post_link('Modifier', '<p>', '</p>'); ?>
 				<p>
-					This entry was posted
-					<?php /* This is commented, because it requires a little adjusting sometimes.
-					You'll need to download this plugin, and follow the instructions:
-					http://binarybonsai.com/wordpress/time-since/ */
-					/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
-					on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
-					and is filed under <?php the_category(', ') ?>.
-					You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
-
+					Cet article a été publié le <?php the_time('l j F Y') ?> à <?php the_time("G\hi") ?>
+					dans la catégorie <?php the_category(', ') ?>.
+					Vous pouvez suivre les commentaires de cet article en utilisant <?php post_comments_feed_link('le flux RSS dédié'); ?>.
+					
 					<?php if ( comments_open() && pings_open() ) {
 					// Both Comments and Pings are open ?>
-					You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
+					Vous pouvez <a href="#respond">laisser un commentaire sur cet article</a>, ou <a href="<?php trackback_url(); ?>" rel="trackback">répondre depuis votre site (trackback)</a>.
 
 					<?php } elseif ( !comments_open() && pings_open() ) {
 					// Only Pings are Open ?>
@@ -54,8 +45,12 @@ get_header(); ?>
 					// Neither Comments, nor Pings are open ?>
 					Both comments and pings are currently closed.
 					
-					<?php } edit_post_link('Edit this entry','','.'); ?>
+					
+					
+					<?php } ?>
 				</p>
+				<?php the_tags( '<p>Tags : ', ', ', '</p>'); ?>
+				<?php //edit_post_link('Modifier','<p>','</p>'); ?>
 			</footer>	
 		</article>
 		
