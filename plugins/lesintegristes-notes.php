16da857ea7085e6c2dad6875df14fcab9ac32acc
Pierre Bertet <bonjour@pierrebertet.net>
2012-09-17 16:37:07 +0200

    Notes plugin: unicode

--- plugins/lesintegristes-notes.php
+++ plugins/lesintegristes-notes.php
@@ -6,28 +6,22 @@ Description: Remplit automatiquement le titre des notes.
 Author: Pierre Bertet
 Version: 1.0.0
 Author URI: http://www.pierrebertet.net
-*/ 
+*/
 
 add_action('save_post', 'lesintegristes_notes_title_intercept', 10, 2);
 
 /* Si un post appartient à la catégorie Note, le titre et le slug sont automatiquement remplis. */
 function lesintegristes_notes_title_intercept($postID, $post) {
-	
-	//$cur_post_id = ( wp_is_post_revision($postID) )? $post->post_parent : $postID;
-	//$parent_post = get_post($cur_post_id);
-	
-	if (isset($_POST["post_category"]) && in_array("31", $_POST["post_category"])) {
-		
-		$post_excerpt   = substr(strip_tags($post->post_content), 0, 50);
-		$new_post_title = "Note&nbsp;: " . $post_excerpt . "&nbsp;[…]";
-		$new_post_name  = sanitize_title($post_excerpt);
-		
-		if ( $post->post_title !== $new_post_title && $post->post_name !== $new_post_name ) {
-			wp_update_post(array(
-				"ID" => $postID,
-				"post_title" => $new_post_title,
-				"post_name" => $new_post_name
-			));
-		}
-	}
+  if (isset($_POST["post_category"]) && in_array("31", $_POST["post_category"]) && !wp_is_post_revision($postID)) {
+    $post_excerpt   = mb_substr(strip_tags($post->post_content), 0, 50, 'utf-8');
+    $new_post_title = "Note&nbsp;: " . $post_excerpt . "&nbsp;[…]";
+    $new_post_name  = sanitize_title($post_excerpt);
+    if ( $post->post_title !== $new_post_title && $post->post_name !== $new_post_name ) {
+      wp_update_post(array(
+        "ID" => $postID,
+        "post_title" => $new_post_title,
+        "post_name" => $new_post_name
+      ));
+    }
+  }
 }
